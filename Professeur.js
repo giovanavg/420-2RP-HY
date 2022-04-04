d0cef8cb8fb147960e1d0bc3cf23c428958e0a79
Giovana Velarde <giovanavg@gmail.com>
2022-04-04 14:42:58 -0400

    Branche v1.2 ajout classe Cours

--- Professeur.js
+++ Professeur.js
@@ -3,6 +3,16 @@
  * Auteur : Giovana Velarde
  * Date : 05 février 2021
  */
+
+class Cours{
+    constructor(code, nom){
+        this.code = code;
+        this.nom = nom;
+    }
+    lireInformation(){
+        return this.code + " " + this.nom;
+    }
+}
 /**
  * Cette classe représente un employé d'une Université
  */
@@ -22,7 +32,7 @@ class EmployeUniversite {
         return this.prenom + ", " + this.nom;
     }
 
-    information() {
+    lireInformation() {
         return this.numeroEmploye + " - "
             + this.nomComplet() +
             " - Ancienneté: " + this.anneesExperience;
@@ -39,27 +49,43 @@ class EmployeUniversiteProfesseur extends EmployeUniversite {
         this.coursEnseigne = coursEnseigne;
     }
 
+    lireInformation(){
+        return super.lireInformation() + " " + this.salaire + 
+        " Cours enseigne: {" + this.lireCoursEnseigne() + "}";
+    }
+
+    lireCoursEnseigne(){
+        var s = "";
+        for(let i=0; i<this.coursEnseigne.length; i++){
+            s += this.coursEnseigne[i].lireInformation() + ", "
+        }
+        return s;
+    }
+
     ajouterUnCours(nouveauCours) {
         this.coursEnseigne[this.coursEnseigne.length] = nouveauCours;
     }
 }
 
 //Code pour tester la classe d'un employé universitaire
-employe = new EmployeUniversite('Pierre', 'Martel', 'E0004', 5);
+var employe = new EmployeUniversite('Pierre', 'Martel', 'E0004', 5);
 employe.augmenterAnciennete();
 console.log(employe.nomComplet());
 console.log(employe.anneesExperience);
 
+var cours1 = new Cours("2RP", "Prog. Client-Serveur");
+var cours2 = new Cours("2DP", "Prog. Orientée Objet");
+
 //Code pour tester la classe d'un professeur universitaire
-prof = new EmployeUniversiteProfesseur('Carmel', 'Simon', 'P0007', 4, 65000, []);
+var prof = new EmployeUniversiteProfesseur('Carmel', 'Simon', 'P0007', 4, 65000, [cours1]);
 prof.augmenterAnciennete();
 console.log(prof.nomComplet());
 console.log(prof.anneesExperience);
 console.log(prof.salaire);
 console.log(prof.coursEnseigne);
-console.log(prof.information());
+console.log(prof.lireInformation());
 
-prof.ajouterUnCours('2RP');
+prof.ajouterUnCours(cours2);
 console.log(prof.coursEnseigne);
-prof.ajouterUnCours('CG2');
-console.log(prof.coursEnseigne);
+
+console.log(prof.lireInformation());
